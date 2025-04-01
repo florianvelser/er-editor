1dcd81e11afc335d5e93efc9a87eadb369974025
Florian Velser <florian.velser@weidmueller.com>
2025-04-01 14:43:44 +0200

    zoom slider implementation

--- src/index.js
+++ src/index.js
@@ -9,6 +9,22 @@ const er_diagram = new ERDiagram(
     document.documentElement.clientHeight - topOffset - bottomOffset
 )
 
+const zoomRange = document.getElementById('zoom-level');
+const zoomText = document.getElementById('zoom-text');
+
+zoomRange.value = 50;
+
+zoomRange.addEventListener("input", function() {
+    er_diagram.setZoom(zoomRange.value);
+}, false);
+
+er_diagram.onZoom = function(zoom) {
+    const minLog = Math.log10(0.1);
+    const maxLog = Math.log10(10);
+    zoomRange.value = ((Math.log10(zoom) - minLog) / (maxLog - minLog)) * 100;
+    zoomText.innerText = Math.round(zoom * 100) + "%";
+}
+
 window.addEventListener("resize", function () {
     er_diagram.setDimensions(
         this.window.innerWidth,
